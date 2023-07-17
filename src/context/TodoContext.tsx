b3c88af05f5c33772ace8ce11cea2724f901ce34
Louis Claremont <louisclaremont@MASTUDENT-Louis-Claremont-C17GMB3FQ6L4.local>
2023-07-17 16:51:05 +0100

    TodoList component utilises useTodo hook for accessing the todos array

--- src/context/TodoContext.tsx
+++ src/context/TodoContext.tsx
@@ -3,19 +3,32 @@ import { nanoid } from 'nanoid'
 import { useLocalStorage } from 'usehooks-ts'
 
 interface TodoContextProps {
-  todos: string[],
+  todos: Todo[],
   addTodo: (text: string) => void
 }
+
+export interface Todo {
+  id: string
+  text: string
+  status: 'undone' | 'completed'
+}
+
 export const TodoContext = createContext<TodoContextProps | undefined>(
   undefined,
 )
 
 export const TodoProvider = (props: { children: React.ReactNode }) => {
-  const [todos, setTodos] = useState<string[]>([])
+  const [todos, setTodos] = useState<Todo[]>([])
 
   // ::: ADD NEW TODO :::
   const addTodo = (text: string) => {
-    setTodos([...todos, text])
+    const newTodo: Todo = {
+      id: nanoid(),
+      text,
+      status: 'undone',
+    }
+
+    setTodos([...todos, newTodo])
   }
 
   const value: TodoContextProps = {
